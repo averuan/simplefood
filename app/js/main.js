b4c1277b9173009428c4490ddc0c0a2604d2812b
averuan <montekristo686@mail.ru>
2021-12-31 15:26:54 +0400

    filter price

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,6 @@
 $(function(){
+
+  $('.select-style').styler();
   
   $(window).scroll(function () {
 
@@ -41,6 +43,8 @@ $(function(){
     rows: 0,
   });
 
+  
+
 
 
 
@@ -57,8 +61,66 @@ $(function(){
 
   $('.reviews__inner').css('display','flex');
 
-
-
-
   var Mixer = mixitup( '.range__content' );
+});
+
+
+
+var $range = $(".filter-price__input"),
+    $inputFrom = $(".filter-price__from"),
+    $inputTo = $(".filter-price__to"),
+    instance,
+    min = 0,
+    max = 99999,
+    from = 0,
+    to = 0;
+
+$range.ionRangeSlider({
+	skin: "round",
+    type: "double",
+    //min: min,
+    //max: max,
+    //from: 200,
+    //to: 800,
+    onStart: updateInputs,
+    onChange: updateInputs
+});
+instance = $range.data("ionRangeSlider");
+
+function updateInputs (data) {
+	from = data.from;
+    to = data.to;
+    
+    $inputFrom.prop("value", from);
+    $inputTo.prop("value", to);	
+}
+
+$inputFrom.on("input", function () {
+    var val = $(this).prop("value");
+    
+    // validate
+    if (val < min) {
+        val = min;
+    } else if (val > to) {
+        val = to;
+    }
+    
+    instance.update({
+        from: val
+    });
+});
+
+$inputTo.on("input", function () {
+    var val = $(this).prop("value");
+    
+    // validate
+    if (val < from) {
+        val = from;
+    } else if (val > max) {
+        val = max;
+    }
+    
+    instance.update({
+        to: val
+    });
 });
--- gulpfile.js
+++ gulpfile.js
@@ -54,6 +54,9 @@ function scripts() {
       'node_modules/jquery/dist/jquery.js',
       'node_modules/slick-carousel/slick/slick.js',
       'node_modules/mixitup/dist/mixitup.js',
+      'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+      'node_modules/jquery-form-styler/dist/jquery.formstyler.js',
+      
       'app/js/main.js'
     ])
     .pipe(concat('main.min.js'))
