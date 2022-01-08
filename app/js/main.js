2162123219c3f6a39430a8a7a2a5fd3cb5f76bba
averuan <montekristo686@mail.ru>
2022-01-08 11:36:15 +0400

    adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -10,6 +10,28 @@ $(function(){
       $(".header").removeClass("header--fix");
     }
   });
+
+
+    $(".header__inner a, .hero__content a, footer a").on("click", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+ 
+      top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1500);
+  });
+
+  $('.burger').click(function (event) {
+    $('.burger').toggleClass('burger--active');
+    $('.menu').toggleClass('menu--active');
+    //$('.menu__link').toggleClass('menu__link--active');
+    $('body').toggleClass('lock');
+  });
+
+  $('.menu__link').click(function (event) {
+    $('.menu').toggleClass('menu--active');
+    $('body').toggleClass('lock');
+    $('.burger').toggleClass('burger--active');
+  });
   
 
   
